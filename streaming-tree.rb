b5c91dddaae093038e9368d90849a4b6919e517c
Bruno Marques <bomarques@inf.ufrgs.br>
2012-06-16 19:14:22 +0000

    Detecção de RTSP (nem sei se funciona)

--- streaming-tree.rb
+++ streaming-tree.rb
@@ -1,5 +1,27 @@
 class StreamingTree < Controller
 	def start
 		info "Iniciando módulo streaming-tree"
+		@urls = {
+			"rtsp://127.0.0.1/stream.sdp" => "example"
+		}
+		@groups = Hash.new
+	end
+
+	def packet_in dpid, message
+		if message.udp?
+			lines = message.udp_payload.lines('\r\n')
+			if lines[0] =~ /^([A-Z]+) (.*) RTSP\/1\.0\r\n$/
+				method = $1
+				uri = $2
+				info method
+				info uri
+				if @urls.member?(uri)
+					new_payload = [lines[0]]
+					lines[1..-1].each do |line|
+						info line
+					end
+				end
+			end
+		end
 	end
 end
