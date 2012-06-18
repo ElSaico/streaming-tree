ad7d754e9eb60b3dde940d23d4093bbb6236ec17
Bruno Marques <bomarques@inf.ufrgs.br>
2012-06-17 19:45:36 -0700

    Implementação incompleta de RPF

--- streaming-tree.rb
+++ streaming-tree.rb
@@ -3,42 +3,80 @@ require "set"
 class StreamingTree < Controller
 	def start
 		info "Iniciando módulo streaming-tree"
+		# portas de um switch
+		@ports = Hash.new
+		# switches de última milha pra cada grupo
 		@groups = Hash.new do | hash, key |
 			hash[key] = Set.new
 		end
+		# switches que já possuem fluxo para um determinado grupo
+		@datapath_in = Hash.new do | hash, key |
+			hash[key] = Set.new
+		end
+		# fluxos de saída de um switch em um determinado grupo
+		@datapath_out = Hash.new do | hash, key |
+			hash[key] = Hash.new do | h2, k2 |
+				h2[k2] = Set.new
+			end
+		end
+	end
+
+	def switch_ready dpid
+		send_message dpid, FeaturesRequest.new
+	end
+
+	def features_reply message
+		@ports[message.datapath_id] = message.ports.collect{|each| each.number}
 	end
 
 	def packet_in dpid, message
 		if message.igmp?
-			handle_igmp message
+			handle_igmp dpid, message
 		else
-			group = members message.ipv4_daddr
-			if group.empty?
+			group = message.ipv4_daddr
+			if members(group).empty?
 				flood_out dpid, message
 			else
-				# RPF!
+				if @datapath_in.member? dpid
+					delete_flow dpid, group, message
+				else
+					@datapath_in[dpid] = true
+					flood_mod dpid, group, message
+				end
 			end
+		end
+	end
+
+	def flow_removed dpid, message
+		if message.reason != OFPRR_DELETE
+			@datapath_in[dpid] = false
 		end
 	end
 
 	private
 
-	def handle_igmp message
+	def handle_igmp dpid, message
 		group = members message.igmp_group
-		source = message.macsa
 		if message.igmp_v2_membership_report?
-			group.add(source)
+			group.add(dpid)
 		elsif message.igmp_v2_leave_group?
-			group.delete(source)
+			group.delete(dpid)
 		end
 	end
 
-	def flood_mod dpid, message
-		send_flow_mod_add(
-			dpid,
-			:match => ExactMatch.from(message),
-			:actions => ActionOutput.new( :port => OFPP_FLOOD )
-		)
+	# TODO: se porta aponta para host final que não é membro do grupo (como descobrir?), apaga/não adiciona fluxo
+	def flood_mod dpid, group, message
+		@ports[dpid].each{ |port|
+			if port != message.in_port
+				send_flow_mod_add(
+					dpid,
+					:match => ExactMatch.from(message),
+					:actions => ActionOutput.new( :port => port )
+				)
+				@datapath_out[dpid][group].add(port)
+			end
+		}
+		flood_out dpid, message
 	end
 
 	def flood_out dpid, message
@@ -47,6 +85,12 @@ class StreamingTree < Controller
 			:packet_in => message,
 			:actions => ActionOutput.new( :port => OFPP_FLOOD )
 		)
+	end
+
+	def delete_flow dpid, group, message
+		if @datapath_out[dpid][group].empty?
+			# apaga fluxo de chegada (recursivamente)
+		end
 	end
 
 	def members group
